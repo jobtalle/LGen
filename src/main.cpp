fc1dbb86c562d51a3a808a20747e2dbf02a356d5
jobtalle <jobtalle@hotmail.com>
2019-02-11 15:49:31 +0100

    Created LParse tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <random>
 
 #include "console/console.h"
 #include "monitor/monitor.h"
 
+#include "rule.h"
+
 int main(int argc, char **argv) {
+	std::random_device randomDevice;
+	std::mt19937 randomizer(randomDevice());
+
+	const LParse::Rule r1(
+		LParse::Sentence({ LParse::Token('A') }),
+		LParse::Sentence({ LParse::Token('A'), LParse::Token('B'), LParse::Token('B') }));
+	const LParse::Rule r2(
+		LParse::Sentence({ LParse::Token('B'), LParse::Token('B') }),
+		LParse::Sentence({ LParse::Token('A') }));
+	const LParse::Rule r3(
+		LParse::Sentence({ LParse::Token('B'), LParse::Token('B') }),
+		LParse::Sentence({ LParse::Token('C') }));
+
+	LParse::Sentence axiom({ LParse::Token('A'), LParse::Token('B'), LParse::Token('A') });
+
+	std::cout << "Axiom: ";
+	axiom.print(std::cout);
+	std::cout << std::endl;
+
+	for(int i = 0; i < 5; ++i) {
+		std::cout << "Iteration " << i << std::endl;
+
+		axiom.apply({ r1, r2, r3 }, randomizer);
+
+		axiom.print(std::cout);
+		std::cout << std::endl;
+	}
+
 	LGen::Monitor monitor("Monitor");
 	LGen::Console console(&monitor);
 

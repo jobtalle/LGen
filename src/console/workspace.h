4b2650a97a2e2731a43936bac7075a6a8e35385f
jobtalle <jobtalle@hotmail.com>
2019-04-15 12:05:34 +0200

    Thread count is the number of threads given by the system by default

--- src/console/workspace.h
+++ src/console/workspace.h
@@ -9,15 +9,17 @@
 namespace LGen {
 	class Workspace final {
 	public:
+		Workspace();
+
 		LParse::Randomizer randomizer;
 		std::unique_ptr<LParse::System> system;
 		std::unique_ptr<Environment> environment;
 		std::unique_ptr<Simulation> simulation;
 		size_t systemIterations = ITERATIONS_DEFAULT;
-		size_t threadCount = THREAD_COUNT_DEFAULT;
+		size_t threadCount;
 
 	private:
 		static const size_t ITERATIONS_DEFAULT = 5;
-		static const size_t THREAD_COUNT_DEFAULT = 1;
+		static const size_t THREAD_COUNT_DEFAULT = 8;
 	};
 }
